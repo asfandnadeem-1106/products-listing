1c064fb717ae8800302c7dc0487f87a94cf07240
Asfand Nadeem <asfandnadeem116@gmail.com>
2024-05-09 11:36:25 +0500

    added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Container } from "@mui/material";
 import Navbar from "./Components/Navbar";
 import Product from "./Components/Product";
+import Footer from "./Components/Footer";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Container component="section" maxWidth={"lg"}>
         <Navbar/>
         <Product/>
+        <Footer/>
       </Container>
     </main>
   );
---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <section className="footer">
+                <ul>
+                    <li><p>Product</p></li>
+                    <li class="footer-links">Coding</li>
+                    <li class="footer-links">Prototyping</li>
+                    <li class="footer-links">Knowledge Sharing</li>
+                    <li class="footer-links">Feedback</li>
+                    <li class="footer-links">What’s New</li>
+                </ul>
+                <ul>
+                    <li><p>Explore</p></li>
+                    <li class="footer-links">Featured Sandboxes</li>
+                    <li class="footer-links">Search Sandboxes</li>
+                </ul>
+                <ul>
+                    <li><p>For</p></li>
+                    <li class="footer-links">Individuals</li>
+                    <li class="footer-links">Teams</li>
+                    <li class="footer-links">Enterprise</li>
+                </ul>
+                <ul>
+                    <li><p>About</p></li>
+                    <li class="footer-links">Pricing</li>
+                    <li class="footer-links">Company</li>
+                    <li class="footer-links">Blog</li>
+                    <li class="footer-links">Podcasts</li>
+                    <li class="footer-links">Careers</li>
+                    <li class="footer-links">Terms Of Use</li>
+                    <li class="footer-links">Privacy Policy</li>
+                </ul>
+                <ul>
+                    <li><p>Support</p></li>
+                    <li class="footer-links">Documentation</li>
+                    <li class="footer-links">Contact Support</li>
+                    <li class="footer-links">Status</li>
+                </ul>
+            </section>
+        </footer>
+
+
+    );
+};
+
+export default Footer;
