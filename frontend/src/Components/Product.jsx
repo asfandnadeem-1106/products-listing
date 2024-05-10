88e3937a7c5bb7f3a977e9a7f28a8a91cd1bdba6
Asfand Nadeem <asfandnadeem116@gmail.com>
2024-05-10 22:09:37 +0500

    Show static data in case of timeout or error

--- frontend/src/Components/Product.jsx
+++ frontend/src/Components/Product.jsx
@@ -7,14 +7,34 @@ const Product = () => {
     // setSpinner(true)
     const [productData, setProduct] = useState("");
     useEffect(() => {
-        fetch("https://products-listing.onrender.com/api/products/663ba6ef8641a65fe20887d8")
-            .then((res) => res.json())
-            .then((data) => {
-                setProduct(data.product)
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://products-listing.onrender.com/api/products/663ba6ef8641a65fe20887d8');
+                const jsonData = await response.json();
+                setProduct(jsonData.product)
+            } catch (error) {
+                // show static data in case of error
                 setSpinner(false)
+                setProduct({"name": "Samsung s23",
+                "title": "Samsung Smartphone",
+                "price": "1000$",
+                "description": "The Samsung S23: A sleek and powerful device, seamlessly blending cutting-edge technology with elegant design. Experience vivid visuals, lightning-fast performance, and unparalleled innovation in the palm of your hand. Redefining what's possible, it's more than just a phone—it's a gateway to a world of limitless possibilities.",
+                "image": "https://images.samsung.com/pk/smartphones/galaxy-s23/buy/kv_group_MO_v2.jpg"})
             }
-        );
-    }, [])
+        };
+        // Show static data in case of timeout
+        const timeoutId = setTimeout(() => {
+            setSpinner(false)
+            setProduct({"name": "Samsung s23",
+            "title": "Samsung Smartphone",
+            "price": "1000$",
+            "description": "The Samsung S23: A sleek and powerful device, seamlessly blending cutting-edge technology with elegant design. Experience vivid visuals, lightning-fast performance, and unparalleled innovation in the palm of your hand. Redefining what's possible, it's more than just a phone—it's a gateway to a world of limitless possibilities.",
+            "image": "https://images.samsung.com/pk/smartphones/galaxy-s23/buy/kv_group_MO_v2.jpg"})
+        }, 5000); // 5000 milliseconds
+        fetchData();
+        return () => clearTimeout(timeoutId); // Clean up timeout if component unmounts
+    }, []); // Empty dependency array means this effect runs only once on mount
+
     return (
         <section className="core">
             {spinner && ( <div className="loader"> <CircularProgress/>  </div>)}
